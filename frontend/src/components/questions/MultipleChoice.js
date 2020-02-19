8ef5d52ed163219a93e669a4888705030248459d
bozman454 <bosmanwillem94@gmail.com>
2020-02-19 12:20:56 -0500

    added material UI to question cards

--- frontend/src/components/questions/MultipleChoice.js
+++ frontend/src/components/questions/MultipleChoice.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 export default function MultipleChoice(props){
   return(
-     <div>
-      <p>Question ID: {props.question_id}</p>
-      <p>Question: {props.question_content}</p>
-      <p>Responses: {returnRs(props.question_response)}</p>
-      <p>Selected: {props.question_selection}</p>
-     </div>
+     <Card>
+      <CardContent>
+
+        <Typography variant="h5"component="h2">
+        Question: {props.question_content}
+        </Typography>
+        <Typography variant="h7" component="p">
+        Responses: {returnRs(props.question_response)}
+        </Typography>
+        <Typography variant="h7"  component="p2">
+        Selected Response: {props.question_selection}
+        </Typography>
+      </CardContent>
+     </Card>
   )
 }
 
 function returnRs(rs){
-  return rs.map((r) => <p>{r}</p>)
+  return rs.map((r) => r = r + " ")
 }
--- frontend/src/components/questions/ShortAnswer.js
+++ frontend/src/components/questions/ShortAnswer.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 
 export default function ShortAnswer(props){
 
   return (
-    <div>
-    <p>Question ID: {props.question_id}</p>
-     <p>Question: {props.question_content}</p>
-     <p>Answer: {props.question_response}</p>
-     </div>
+    <Card>
+    <CardContent>
+      <Typography variant="h5"component="h2">
+        Question: {props.question_content}
+      </Typography>
+      <Typography variant="h7"component="p">
+        Response: {props.question_response}
+      </Typography>
+    </CardContent>
+    </Card>
   )
 }
